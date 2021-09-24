bb3d9e4e4f507405284089df30bc8bd8674bdc7c
OverdueWeevil2 <61hasanabdullahuygun@gmail.com>
2021-09-24 17:25:57 +0300

    Update hammer-beta.py

--- hammer-beta.py
+++ hammer-beta.py
@@ -16,18 +16,6 @@ def user_agent():
 	uagent.append("Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.7 (KHTML, like Gecko) Comodo_Dragon/16.1.1.0 Chrome/16.0.912.63 Safari/535.7")
 	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
 	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1")
-	uagent.append("DDoS1")
-	uagent.append("DDoS2")
-	uagent.append("DDoS3")
-	uagent.append("DDoS4")
-	uagent.append("DDoS5")
-	uagent.append("DDoS6")
-	uagent.append("DDoS7")
-	uagent.append("DDoS8")
-	uagent.append("DDoS9")
-	uagent.append("DDoS10")
-	uagent.append("DDoS11")
-	uagent.append("DDoS12")
 	return(uagent)
 
 
